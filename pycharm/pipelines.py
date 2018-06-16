7a66bf78e36d2ebbf8a8dd60802d253357dcb25e
Li Li <fywest2109@hotmail.com>
2018-06-16 22:52:22 +0300

    challenge17

--- pycharm/pipelines.py
+++ pycharm/pipelines.py
@@ -5,32 +5,22 @@
 
 from datetime import datetime
 from sqlalchemy.orm import sessionmaker
-from .models import Course,User,Repository,engine
+from .models import Repository,engine
 from scrapy.exceptions import DropItem
-from .items import CourseItem, UserItem,RepositoryItem
+from .items import RepositoryItem
 
 class ShiyanlouPipeline(object):
     def process_item(self, item, spider):
-        if isinstance(item, CourseItem):
-            self._process_course_item(item)
-        elif isinstance(item, UserItem):
-            self._process_user_item(item)
-        elif isinstance(item, RepositoryItem):
+        if isinstance(item, RepositoryItem):
             self._process_repository_item(item)
+
         return item
-
-    def _process_course_item(self, item):
-        item['students'] = int(item['students'])
-        self.session.add(Course(**item))
-
-    def _process_user_item(self, item):
-        item['level'] = int(item['level'][1:])
-        item['join_date'] = datetime.strptime(item['join_date'].split()[0], '%Y-%m-%d').date()
-        item['learn_courses_num'] = int(item['learn_courses_num'])
-        self.session.add(User(**item))
 
     def _process_repository_item(self, item):
         item['update_time'] = datetime.strptime(item['update_time'].split()[0], '%Y-%m-%dT%H:%M:%SZ')
+        item['commits'] = int(item['commits'])
+        item['branches'] = int(item['branches'])
+        item['releases'] = int(item['releases'])
         self.session.add(Repository(**item))
     
     def open_spider(self, spider):
